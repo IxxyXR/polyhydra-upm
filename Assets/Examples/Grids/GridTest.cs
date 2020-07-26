b104be926df24ae455b4a461deba9118ed4870c9
Andy Baker <andy@andybak.net>
2020-07-26 14:48:28 +0100

    Clean up and namespacing changes. Move more stuff into common code

---
+++ Assets/Examples/Grids/GridTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GridTest : MonoBehaviour
+{
+    public PolyHydraEnums.GridTypes GridType;
+    public PolyHydraEnums.GridShapes GridShape;
+    [Range(1,40)] public int width = 4;
+    [Range(1,40)] public int depth = 3;
+
+    void Start()
+    {
+        Generate();
+    }
+
+    private void OnValidate()
+    {
+        Generate();
+    }
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    {
+        var poly = Grids.Grids.MakeGrid(GridType, GridShape, width, depth);
+        var mesh = PolyMeshBuilder.BuildMeshFromConwayPoly(poly, false, null, PolyHydraEnums.ColorMethods.BySides);
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+}
