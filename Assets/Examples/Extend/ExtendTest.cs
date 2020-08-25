831b576d8dde04a1c91704e37fa989cd93d64c65
andybak <andy@andybak.net>
2020-08-25 10:04:15 +0100

    Add angle param to ExtendBoundaries

--- Assets/Examples/Extend/ExtendTest.cs
+++ Assets/Examples/Extend/ExtendTest.cs
@@ -11,7 +11,15 @@ public class ExtendTest : MonoBehaviour
     [Range(1,40)] public int width = 4;
     [Range(1,40)] public int depth = 3;
     public float amount = 1;
+    public float angle = 0;
+    public FaceSelections removeSelection = FaceSelections.Inner;
     public bool ColorBySides;
+
+    public bool ApplyOp;
+    public Ops op;
+    public FaceSelections facesel;
+    public float opAmount = 0;
+    public float op2Amount = 0;
 
     void Start()
     {
@@ -28,7 +36,15 @@ public class ExtendTest : MonoBehaviour
     {
         var colorMethod = ColorBySides ? PolyHydraEnums.ColorMethods.BySides : PolyHydraEnums.ColorMethods.ByRole;
         var poly = Grids.Grids.MakeGrid(GridType, GridShape, width, depth);
-        poly = poly.FaceRemove(new OpParams {facesel = FaceSelections.Inner});
+        poly = poly.FaceRemove(new OpParams {facesel = removeSelection});
+        poly = poly.ExtendBoundaries(new OpParams{valueA = amount, valueB = angle});
+        
+        if (ApplyOp)
+        {
+            var o = new OpParams {valueA = opAmount, valueB = op2Amount, facesel = facesel};
+            poly = poly.ApplyOp(op, o);
+        }
+        
         var mesh = PolyMeshBuilder.BuildMeshFromConwayPoly(poly, false, null, colorMethod);
         GetComponent<MeshFilter>().mesh = mesh;
     }
