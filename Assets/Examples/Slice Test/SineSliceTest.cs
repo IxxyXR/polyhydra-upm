5258019e7d88b8a29ef1ec95ba2b9c75e9ce88bb
Andy Baker <andy@andybak.net>
2020-12-17 10:42:07 +0000

    Some new slice examples

---
+++ Assets/Examples/Slice Test/SineSliceTest.cs
@@ -0,0 +1,139 @@
+using Conway;
+using Johnson;
+using UnityEngine;
+using Wythoff;
+
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SineSliceTest : MonoBehaviour
+{
+    public PolyHydraEnums.JohnsonPolyTypes JohnsonPolyType;
+    [Range(3,40)] public int Sides = 4;
+ 
+    public bool ApplyOp;
+    public Ops op0;
+    public FaceSelections op0Facesel;
+    public float op0Amount1 = 0;
+    public float op0Amount2 = 0;
+    public Ops op1;
+    public FaceSelections op1Facesel;
+    public float op1Amount1 = 0;
+    public float op1Amount2 = 0;
+    public Ops op2;
+    public FaceSelections op2Facesel;
+    public float op2Amount1 = 0;
+    public float op2Amount2 = 0;
+    public bool Canonicalize;
+    public PolyHydraEnums.ColorMethods ColorMethod;
+
+    [Space]
+    public Vector3 PositionBefore = Vector3.zero;
+    public Vector3 RotationBefore = Vector3.zero;
+    public Vector3 ScaleBefore = Vector3.one;
+    public Vector3 SliceAngle = Vector3.zero;
+    public float SliceDistance = 0.2f;
+    public float SliceGap = 0.1f;
+    public float SliceStart = -.5f;
+    public float SliceEnd = 0.5f;
+    public bool Cap;
+    public bool Weld;
+    public float AnimationSpeed;
+    
+    public float frequency = 1f;
+    public float amplitude = 1f;
+
+    private ConwayPoly poly;
+
+    void Start()
+    {
+        Generate();
+    }
+    
+    private void OnValidate()
+    {
+        Generate();
+    }
+    
+    private void Update()
+    {
+        if (AnimationSpeed>0)
+        SliceAngle = new Vector3(Mathf.Sin(Time.time*AnimationSpeed + 5f) * 60, Mathf.Sin(Time.time*AnimationSpeed*.7f) * 90, 0);
+        Generate();
+    }
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    {
+        poly = JohnsonPoly.Build(JohnsonPolyType, Sides);
+        poly = poly.Transform(PositionBefore, RotationBefore, ScaleBefore);
+        
+        if (ApplyOp)
+        {
+            var o0 = new OpParams {valueA = op0Amount1, valueB = op0Amount2, facesel = op0Facesel};
+            poly = poly.ApplyOp(op0, o0);
+        }
+
+        if (Canonicalize)
+        {
+            poly = poly.Canonicalize(0.01, 0.01);
+        }
+        
+        poly.Recenter();
+
+        ConwayPoly DoSlices(ConwayPoly input, Vector3 axis)
+        {
+            var result = new ConwayPoly();
+            float offset = SliceStart;
+
+            int failsafe = 100;
+            
+            do
+            {
+                offset += SliceDistance;
+                var planePos = new Vector3(offset*axis.x, offset*axis.y, offset*axis.z);
+                var sliced = input.SliceByPlane(new Plane(Quaternion.Euler(SliceAngle) * axis, planePos), Cap);
+                var gapVector = new Vector3(-SliceGap*axis.x, -SliceGap*axis.y, -SliceGap*axis.z);
+                result = result.Transform(gapVector);
+                var lerp = Mathf.InverseLerp(SliceStart, SliceEnd, offset);
+                var shift = new Vector3(Mathf.Sin(lerp*frequency)*amplitude, 0, 0);
+                result.Append(sliced.bottom.Transform(shift));
+                input = sliced.top;
+                failsafe--;
+            } while (offset < SliceEnd && failsafe > 0);
+
+            result.Append(input);
+            if (Weld)
+            {
+                result = result.Weld(0.0001f);
+            }
+            return result;
+        }
+
+        var output = new ConwayPoly();
+        output.Append(DoSlices(poly, Vector3.up));
+
+        if (ApplyOp)
+        {
+            var o1 = new OpParams {valueA = op1Amount1, valueB = op1Amount2, facesel = op1Facesel};
+            output = output.ApplyOp(op1, o1);
+            if (Weld)
+            {
+                poly = poly.Weld(0.0001f);
+            }
+            var o2 = new OpParams {valueA = op2Amount1, valueB = op2Amount2, facesel = op2Facesel};
+            output = output.ApplyOp(op2, o2);
+        }
+        
+        output.Recenter();
+
+        var mesh = PolyMeshBuilder.BuildMeshFromConwayPoly(output, false, null, ColorMethod);
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+    
+    void OnDrawGizmos () {
+        // GizmoHelper.DrawGizmos(poly, transform, true, false, false, false);
+    }
+
+
+}
---
+++ Assets/Examples/Slice Test/SliceSegmentTest.cs
@@ -0,0 +1,113 @@
+﻿using Conway;
+using UnityEngine;
+using Wythoff;
+
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SliceSegmentTest : MonoBehaviour
+{
+    public PolyTypes PolyType;
+    [Range(1,40)] public int PrismP = 4;
+    [Range(1,40)] public int PrismQ = 4;
+
+    public bool ApplyOp;
+    public Ops op1;
+    public FaceSelections op1Facesel;
+    public float op1Amount1 = 0;
+    public float op1Amount2 = 0;
+    public Ops op2;
+    public FaceSelections op2Facesel;
+    public float op2Amount1 = 0;
+    public float op2Amount2 = 0;
+    public bool Canonicalize;
+    public Vector3 Position = Vector3.zero;
+    public Vector3 Rotation = Vector3.zero;
+    public Vector3 Scale = Vector3.one;
+    public PolyHydraEnums.ColorMethods ColorMethod;
+
+    [Space]
+    public float animateSlice = 0;
+    public bool includeTop = true;
+    public bool includeBottom = true;
+    public bool Weld;
+    public bool Cap;
+    public Vector3 SlicePosition;
+    public Vector3 SliceRotation;
+    public Vector3 SegmentTransform;
+    public float SegmentHeight;
+
+    private ConwayPoly poly;
+    
+    void Start()
+    {
+        Generate();
+    }
+
+    private void OnValidate()
+    {
+        Generate();
+    }
+    
+    private void Update()
+    {
+        Generate();
+    }
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    {
+        var wythoff = new WythoffPoly(PolyType, PrismP, PrismQ);
+        wythoff.BuildFaces();
+        poly = new ConwayPoly(wythoff);
+        if (ApplyOp)
+        {
+            var o1 = new OpParams {valueA = op1Amount1, valueB = op1Amount2, facesel = op1Facesel};
+            poly = poly.ApplyOp(op1, o1);
+        }
+
+        if (Canonicalize)
+        {
+            poly = poly.Canonicalize(0.01, 0.01);
+        }
+        
+        poly = poly.Transform(Position, Rotation, Scale);
+
+        var pos = SlicePosition;
+        if (animateSlice > 0)
+        {
+            pos.y = .1f + transform.position.y + ((Time.time % 2f) * animateSlice);
+        }
+
+        var rot = Quaternion.Euler(SliceRotation) * Vector3.up;
+        var slicePlane = new Plane(rot, pos);
+        var result = poly.SliceByPlane(slicePlane, Cap, includeTop, includeBottom);
+        poly = result.bottom;   
+        var slicePoint = slicePlane.normal * (-slicePlane.distance + SegmentHeight);
+        slicePlane.SetNormalAndPosition(slicePlane.normal, slicePoint);
+        var (top, segment, _) = result.top.SliceByPlane(slicePlane, Cap, includeTop, includeBottom);
+        segment = segment.Transform(SegmentTransform);
+        poly.Append(top);
+        poly.Append(segment);
+        
+        if (Weld)
+        {
+            poly = poly.Weld(0.0001f);
+        }
+        
+        if (ApplyOp)
+        {
+            var o2 = new OpParams {valueA = op2Amount1, valueB = op2Amount2, facesel = op2Facesel};
+            poly = poly.ApplyOp(op2, o2);
+        }
+
+        var mesh = PolyMeshBuilder.BuildMeshFromConwayPoly(poly, false, null, ColorMethod);
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+    
+    void OnDrawGizmos () {
+        // GizmoHelper.DrawGizmos(poly, transform, true, false, false, false);
+    }
+
+
+}
