e7919fb9f0d5f479bb2f04819a48d02242fc47d9
Andy Baker <andy@andybak.net>
2021-03-28 13:34:58 +0100

    Refactor tactile grids and move important stuff out of example class.

--- com.ixxy.polyhydra/Grids/Runtime/MultiGrid.cs
+++ com.ixxy.polyhydra/Grids/Runtime/MultiGrid.cs
@@ -350,13 +350,4 @@ namespace Grids
             return new Vector2(x, y);
         }
     }
-
-    public struct Rhomb
-    {
-        public List<Vector2> shape;
-        public int line1;
-        public int parallel1;
-        public int line2;
-        public int parallel2;
-    }
 }
---
+++ com.ixxy.polyhydra/Grids/Runtime/Tactile/Rhomb.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grids
+{
+    public struct Rhomb
+    {
+        public List<Vector2> shape;
+        public int line1;
+        public int parallel1;
+        public int line2;
+        public int parallel2;
+    }
+}
