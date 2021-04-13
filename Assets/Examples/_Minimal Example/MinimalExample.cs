0f3198c684875d924229af784805cc56f52caa26
Andy Baker <andy@andybak.net>
2021-04-13 18:53:49 +0100

    Add a properly commented minimal example

---
+++ Assets/Examples/_Minimal Example/MinimalExample.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Conway;
+using Johnson;
+using UnityEngine;
+
+public class MinimalExample : MonoBehaviour
+{
+    
+    void Start()
+    {
+        // 1. Create a starting shape
+        ConwayPoly poly1 = JohnsonPoly.Build(PolyHydraEnums.JohnsonPolyTypes.Prism, 4);
+        
+        // 2. Apply an operator to the starting shape
+        poly1 = poly1.ApplyOp(Ops.Chamfer, new OpParams(.3f));
+
+        // 3. Create a second shape
+        ConwayPoly poly2 = JohnsonPoly.Build(PolyHydraEnums.JohnsonPolyTypes.Pyramid, 4);
+
+        // 4. Move our second shape down by 0.5
+        poly2 = poly1.Transform(new Vector3(0, .5f, 0));
+
+        // 5. Join the two shapes
+        poly1.Append(poly2);
+
+        // 6. Build and apply the mesh
+        var mesh = PolyMeshBuilder.BuildMeshFromConwayPoly(poly1);
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+    
+   }
